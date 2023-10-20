a8578fd0a2df0c18a793af6c2ce3708a2b136bfa
Najmus73 <shakibnajmus73@gmail.com>
2023-10-21 00:56:55 +0600

    create get method and find by query _id to update a product

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -31,6 +31,19 @@ async function run() {
     await client.connect();
 
     const productCollection = client.db('productDB').collection('product');
+
+    app.get('/product', async(req,res)=>{
+        const cursor = productCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/product/:id',async (req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await productCollection.findOne(query);
+        res.send(result)
+    })
 
     app.post('/product', async(req,res)=>{
          const newProduct = req.body;
